a65f03f683b11fed7f4fb6b6ee57e5c8344c8784
Nde Lucien <chelucien08@gmail.com>
2024-01-17 18:52:26 +0100

    fully fledged out hash table

--- hash-table.js
+++ hash-table.js
@@ -1,35 +1,184 @@
 class HashTable {
-  constructor(size = 10) {
-    this.table = new Array(size);
+  constructor() {
+    this.table = new Array(127);
+    this.size = 0;
+  }
+
+  set(key, value) {
+    this._set(key, value);
+  }
+
+  get(key) {
+    const linkedList = this._get(key);
+    if (!linkedList) {
+      return undefined;
+    }
+
+    return linkedList;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+    const targetItem = this.table[index];
+
+    if (targetItem && targetItem.length) {
+      for (let i = 0; i < targetItem; i++) {
+        if (targetItem[i][0] === key) {
+          targetItem.splice(i, 1);
+          this.size--;
+          return true;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+
+  display() {
+    let returnDisplay = '{';
+    this.table.forEach((values, index) => {
+      const chainedValues = values.map(([key, value]) => `${key}: ${value} `);
+      returnDisplay = returnDisplay + `\n  ${chainedValues}`;
+    });
+    returnDisplay = returnDisplay + '\n}';
+
+    return returnDisplay;
+  }
+
+  keys() {
+    const keys = [];
+    this.table.forEach((values, _index) => {
+      values.map(([key, _value]) => keys.push(key));
+    });
+
+    return keys;
+  }
+
+  values() {
+    const values = [];
+    this.table.forEach((vals, _index) => {
+      vals.map(([_key, value]) => values.push(value));
+    });
+
+    return values;
+  }
+
+  forEach(callback) {
+    return this._loop(callback);
+  }
+
+  map(callback) {
+    return this._loop(callback);
   }
 
   _hash(key) {
     let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i);
+    const prime = 31;
+
+    if (typeof key === 'number') {
+      key = String(key);
+      hash = 1;
     }
-    return hash % this.table.length;
+
+    for (let i = 0; i < key.length; i++) {
+      const charCode = key.charCodeAt(i);
+      hash = (hash * prime + charCode + i) % this.table.length;
+    }
+
+    if (this._getLoadFactor() > 0.7) {
+      this._resize();
+    }
+
+    return hash;
   }
 
-  set(key, value) {
+  _set(key, value) {
     const index = this._hash(key);
-    this.table[index] = [key, value];
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        // Find the key/value pair in the chain
+        if (this.table[index][i][0] === key) {
+          this.table[index][i][1] = value;
+          return;
+        }
+      }
+      // not found, push a new key/value pair
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [];
+      this.table[index].push([key, value]);
+    }
+    this.size++;
   }
 
-  get(key) {
+  _get(key) {
     const index = this._hash(key);
-    return this.table[index][1];
+    const targetList = this.table[index];
+    if (targetList) {
+      for (let i = 0; i < targetList.length; i++) {
+        if (targetList[i][0] === key) {
+          return targetList[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  _getLoadFactor() {
+    return this.size / this.table.length;
+  }
+
+  _loop(callback) {
+    for (const chain of this.table) {
+      if (chain) {
+        for (const [key, value] of chain) {
+          callback(key, value);
+        }
+      }
+    }
+  }
+
+  async _resize() {
+    const newSize = this.table.length * 2;
+    const newTable = new Array(newSize);
+
+    for (const chain of this.table) {
+      if (chain) {
+        for (const [key, value] of chain) {
+          const newIndex = this._hash(key) % newSize;
+          if (!newTable[newIndex]) {
+            newTable[newIndex] = [];
+          }
+          newTable[newIndex].push([key, value]);
+        }
+      }
+    }
+
+    this.table = newTable;
   }
 }
 
 // Testing it works
 
-const m = new HashTable(2);
-m.set('a', 1);
+const m = new HashTable(10);
+m.set('abc', 1);
 m.set('b', 2);
 // this to test if table size is ensured
-m.set('c', 3);
-console.log(m.table);
-const v = m.get('a');
+m.set('bca', 3);
+m.set(31, 3);
+m.set(13, 3);
+console.log(m.display());
+
+const v = m.get('b');
 console.log(v);
-console.log(1 == v);
+console.log(m.keys());
+console.log(m.values());
+// console.log(1 == v);
+
+m.forEach((key, value) => {
+  console.log(key, value);
+});
+
+m.map((key, value) => {
+  console.log(key, value);
+});
